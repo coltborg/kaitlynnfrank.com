7488c723ed1f2dadd03d5273ae439eab097501ad
legendofcolt <coltborg@gmail.com>
2016-05-07 19:06:37 -0500

    finished sidenav functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-DomReady.ready(function () {
+window.onload = function () {
   /* How do I use this effectively? I need to toggle the nav menu on and off
      on and off with a click. But I also need to toggle it base on viewport size.
      Should I handle it with adding/removing classes? Becuase if I add (inline)
@@ -7,13 +7,24 @@ DomReady.ready(function () {
      and the nav doesn't pop out becuase there is an inline style preventing it.
   */
   document.getElementById('js-header-toggle').addEventListener('click', function () {
+    var headerToggleEl = document.getElementById('js-header-toggle');
     var headerEl = document.getElementById('js-header');
-    if (headerEl.classList.contains('right--20r')) {
-      //  this.classList.remove('bad');
-      //  this.classList.add('good');
-      alert('Yep, does contain class');
-    } else {
-      alert('Nope, does NOT contain class');
+
+    if ( headerEl.classList.contains('right--20') ) {
+      headerToggleEl.classList.remove('right-0');
+      headerToggleEl.classList.add('right-20');
+
+      headerEl.classList.remove('right--20');
+      headerEl.classList.add('right-0');
+      return;
+    }
+    if ( headerEl.classList.contains('right-0') ) {
+      headerToggleEl.classList.remove('right-20');
+      headerToggleEl.classList.add('right-0');
+
+      headerEl.classList.remove('right-0');
+      headerEl.classList.add('right--20');
+      return;
     }
   }); // end click event
-});
+};
