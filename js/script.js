a00d31e2539af64cd67af3de2bef7cd00b07be64
legendofcolt <coltborg@gmail.com>
2016-05-22 15:18:15 -0500

    added opacity to main and stopped body from scrolling when nav is clicked

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ function toggleNav(e) {
   e.preventDefault();
   var headerToggleEl = document.getElementById('js-header-toggle');
   var headerEl = document.getElementById('js-header');
+  var bodyEl = document.body;
+  var mainEl = document.getElementById('main');
 
   if ( headerEl.classList.contains('right--20') ) {
     headerToggleEl.classList.remove('right-0');
@@ -13,6 +15,10 @@ function toggleNav(e) {
 
     headerEl.classList.remove('right--20');
     headerEl.classList.add('right-0');
+
+    bodyEl.classList.toggle('ofy-hid');
+    mainEl.classList.toggle('opacity-50');
+
     return;
   }
   if ( headerEl.classList.contains('right-0') ) {
@@ -21,6 +27,10 @@ function toggleNav(e) {
 
     headerEl.classList.remove('right-0');
     headerEl.classList.add('right--20');
+
+    bodyEl.classList.toggle('ofy-hid');
+    mainEl.classList.toggle('opacity-50');
+
     return;
   }
 }
